27c7cbfe7ed71bcb22220275a0e2659dafe8f3a0
hajidah06 <110171796+hajidah06@users.noreply.github.com>
2023-10-19 08:03:31 +0800

    Update daftarpelajarbaru.php

--- daftarpelajarbaru.php
+++ daftarpelajarbaru.php
@@ -22,6 +22,12 @@
   </style>
   <body>
     <center>
+      <ul>
+			<li><a href="#home">Home</a></li>
+			<li><a href="#news">News</a></li>
+			<li><a href="#about">About</a></li>
+		  </ul>
+		  <br><br>
       <img src="img/logokvperd.jpg">
       <br>
       <h2>SISTEM PENDAFTARAN PELAJAR BAHARU</h2>
